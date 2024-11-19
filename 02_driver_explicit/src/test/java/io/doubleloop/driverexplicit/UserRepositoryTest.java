bc8ac7a3b5814e696eb3eeff27207ed10655f485
Matteo Baglini <matteo.baglini@gmail.com>
2024-11-19 13:09:03 +0100

    chore: aggiunti integration test per esercizio 2

---
+++ 02_driver_explicit/src/test/java/io/doubleloop/driverexplicit/UserRepositoryTest.java
@@ -0,0 +1,49 @@
+package io.doubleloop.driverexplicit;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataMongoTest
+@Testcontainers
+class UserRepositoryTest {
+
+  @Container
+  @ServiceConnection
+  private static MongoDBContainer container = new MongoDBContainer("mongo:latest");
+
+  @Autowired
+  private UserRepository userRepository;
+
+  @BeforeEach
+  void setUp() {
+    userRepository.deleteAll();
+  }
+
+  @Test
+  void findByEmailMatch() {
+    userRepository.save(new User("foo@bar.it", "test"));
+    userRepository.save(new User("test@test.it", "test"));
+
+    final var result = userRepository.findByEmail("foo@bar.it");
+
+    assertThat(result.isPresent()).isTrue();
+    assertThat(result.get().getEmail()).isEqualTo("foo@bar.it");
+  }
+
+  @Test
+  void findByEmailNotMatch() {
+    userRepository.save(new User("test@test.it", "test"));
+
+    final var result = userRepository.findByEmail("foo@bar.it");
+
+    assertThat(result.isEmpty()).isTrue();
+  }
+}
